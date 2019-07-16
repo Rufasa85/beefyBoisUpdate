fc1dc202aa86ec14876824b7456f97341f20d688
Joseph Rehfuss <josephrehfuss@D-172-22-194-171.pce.washington.edu>
2019-07-16 12:19:09 -0700

    adding basic file structure

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.get('/',(req,res)=>{
+    db.Host.findAll({}).then(hosts=>{
+        res.render('index',{hosts})
+    })
+})
+
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
