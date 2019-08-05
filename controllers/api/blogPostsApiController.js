2ea9b4688856b7fcdf6c7c8610efdf07ef792400
Joseph Rehfuss <josephrehfuss@D-10-156-9-244.dhcp4.washington.edu>
2019-08-05 14:08:38 -0700

    adding auth

---
+++ controllers/api/blogPostsApiController.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router(); 
+const db  = require('../../models');
+
+router.get('/',(req,res)=>{
+    db.BlogPost.findAll({}).then(blogPosts=>{
+        res.json(blogPosts);
+    })
+});
+
+router.post('/',(req,res)=>{
+    console.log('posting to blog')
+    console.log(req.session);
+    if(!req.session.user){
+        res.redirect('/')
+    }
+    else {
+        db.BlogPost.create({
+          title:req.body.title,
+          body:req.body.body,
+          HostId:req.session.user.id 
+        }).then(newBlogPost=>{
+            res.json(newBlogPost)
+        })
+    }
+})
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 var apiRoutes = require('./api');
 var hostRoutes = require('./hostsController');
+const authRoutes = require("./authController");
 
 router.use('/hosts',hostRoutes)
 router.use('/api',apiRoutes)
+router.use('/auth',authRoutes)
 
 router.get('/',(req,res)=>{
     res.send("index route")
+})
+
+router.get('/addsessions',(req,res)=>{
+    req.session.addedthing = 'yay for sessions';
+    res.redirect('/readsessions');
+})
+
+router.get('/readsessions',(req,res)=>{
+    res.json(req.session)
 })
 
 module.exports = router;
