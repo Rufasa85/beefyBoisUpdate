bf7e4b180ae1d3ad5f84fb4110e5950f63ad55e2
Joseph Rehfuss <josephrehfuss@D-172-22-159-42.dhcp4.washington.edu>
2019-09-16 14:36:26 -0700

    added review routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const hostAPIRoutes = require('./hostsApiController');
 const blogAPIRoutes = require('./blogPostsApiController');
+const reviewAPIRoutes = require('./reviewApiController');
 
 router.use('/hosts',hostAPIRoutes);
 router.use('/blogPosts',blogAPIRoutes);
+router.use('/reviews',reviewAPIRoutes);
 
 router.get('/',(req,res)=>{
     res.send("index API route, MOAR testing")
