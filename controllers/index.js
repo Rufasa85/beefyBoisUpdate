7431dd64e98abe34edc30b87b3d983a91bc1165a
Joseph Rehfuss <josephrehfuss@D-172-22-159-49.dhcp4.washington.edu>
2019-12-12 13:50:59 -0800

    flow set up

--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var apiRoutes = require('./api');
-var hostRoutes = require('./hostsController');
-const authRoutes = require("./authController");
+var frontEndRoutes = require('./frontend/');
 
-router.use('/hosts',hostRoutes)
-router.use('/api',apiRoutes)
-router.use('/auth',authRoutes)
 
-router.get('/',(req,res)=>{
-    res.send("index route")
-})
+router.use('/api',apiRoutes);
+router.use('/',frontEndRoutes);
 
-router.get('/addsessions',(req,res)=>{
-    req.session.addedthing = 'yay for sessions';
-    res.redirect('/readsessions');
-})
-
-router.get('/readsessions',(req,res)=>{
-    res.json(req.session)
-})
 
 module.exports = router;
